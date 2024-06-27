e1ec6d12bdb217e5a702051e49a858d9aa7eece1
SUPAROEK <hopehut8826@gmail.com>
2024-06-27 11:13:10 +0700

    Update

--- blog_view.php
+++ blog_view.php
@@ -1,25 +1,34 @@
 <?php
-include "connect.php";
-$sql = "SELECT * FROM blog";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-// output data of each row
-    while($row = $result->fetch_assoc())
-    {
-        echo "ID: " . $row["id"]. "<br>";
-        echo "Title: $row[title] <br/>";
-        echo "Content: $row[content] <br/>";
-        echo "Poster: $row[poster] <br/>";
-        echo "Date: $row[dmy] <br/>";
-        echo "<a
-        href='blog_delete.php?id=$row[id]'> ลบ </a> ";
-        echo "&nbsp &nbsp";
-        echo "<a
-        href='blog_edit.php?id=$row[id]'> แก้ไข </a> ";
-        echo "<hr/>";
-    }
-} else {
-echo "0 results";
+ob_start();
+session_start();
+echo "<h3> Logged : $_SESSION[email] </h3>";
+if (isset($_SESSION['loggedin'])){
+    echo "<p>";
+    echo "<a href='blogh_view.php'> แสดง Blog </a>";
+    echo "<a href='blogh_from.php'> เพิ่ม Blog </a>";
+    echo "<a href='logout.php'> Logout </a>";
+    echo "</p>";
 }
-$conn->close();
-?>
+ include "connect.php";
+ $sql = "SELECT * FROM blog";
+ $result = $conn->query($sql);
+ if ($result->num_rows > 0) {
+ // output data of each row
+ while($row = $result->fetch_assoc()) 
+{
+    echo "ID: " . $row["id"]. "<br>";
+    echo "Title: $row[title] <br/>";
+    echo "Title: <a href='reply.php?ref= $row[id]'> $row[title] </a><br/>";
+    echo "Content: $row[content] <br/>";
+    echo "Poster: $row[poster] <br/>";
+    echo "Date: $row[dmy] <br/>";
+    echo "<a href='blog_delete.php?id=$row[id]'> ลบ </a> ";
+    echo "&nbsp &nbsp";
+    echo "<a href='blog_edit.php?id=$row[id]'> แก้ไข </a> ";
+    echo "<hr/>";
+ }
+ } else {
+    echo "0 results";
+ }
+ $conn->close();
+ ?>
